7bbe2b9f37a5a1acdc09a57c42baa56e4a326def
youFness <88398775+youFness@users.noreply.github.com>
2025-11-04 15:11:43 +0100

    3000

--- back/src/models/mesure.model.js
+++ back/src/models/mesure.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const mesureSchema = new mongoose.Schema({
   capteur_id: { type: mongoose.Schema.Types.ObjectId, ref: 'capteurs', required: true },
-  type: { type: String, required: true, enum: ['temperature', 'humidite', 'luminosite', 'gaz'] },  
+  type: { type: String, required: true, enum: ['temperature', 'humidite', 'luminosite', 'gaz', 'caz'] },  
   valeur: { type: Number, required: true },
   timestamp: { type: Date, default: Date.now },
 }, { timestamps: false, collection: 'mesures' });
